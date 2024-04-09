44abee136b68051b19718bf85da291bfc4e3a0be
Ada <155854627+CookieLulu1@users.noreply.github.com>
2024-04-08 20:44:22 -0400

    Commit (for mac)

---
+++ FOURCALC/script.js
@@ -0,0 +1,27 @@
+displayed = "";
+buffer = "";
+const display = document.getElementById("display");
+function displayThis(input){
+    input;
+}
+
+function clear() {
+    var display = document.getElementById("display");
+
+    displayed = "";
+    storedNum = "";
+    display.value = displayed;
+}
+
+function calculate() {
+
+}
+
+function backspace() {
+
+}
+
+function undo() {
+
+}
+
